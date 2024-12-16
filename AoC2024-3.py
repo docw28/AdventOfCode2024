416cbef18b9312f53e83f0595efe010c9ed7f330
Dafydd Carver-Williams <102929465+docw28@users.noreply.github.com>
2024-12-16 11:08:17 +0000

    Update AoC2024-3.py

--- AoC2024-3.py
+++ AoC2024-3.py
@@ -1,1 +1,6 @@
+testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
+validChars = ["mul(", ")", "," "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+puzzleInput = list(testInput)
+
+print(puzzleInput)
