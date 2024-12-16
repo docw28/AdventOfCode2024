6d4b1ea5ac659e38ac4f36e880dc2a5cfbbabe0f
Dafydd Carver-Williams <d.carverwilliams@andor.com>
2024-12-16 13:58:05 +0000

    Started day 3

--- AoC2024-3.py
+++ AoC2024-3.py
@@ -1,6 +1,14 @@
 testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
-validChars = ["mul(", ")", "," "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+validChars = ["m", "u", "l", "(", ")", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 puzzleInput = list(testInput)
 
-print(puzzleInput)
+def removeInvalidChars(input):
+    output = []
+    for char in input:
+        if char in validChars:
+            output.append(char)
+    return output
+
+print(removeInvalidChars(puzzleInput))
+
