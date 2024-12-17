33f9fd6ca9703d160fcf2eae5ce9d42fc49ccde1
Dafydd Carver-Williams <d.carverwilliams@andor.com>
2024-12-17 12:22:59 +0000

    finished day 3 part 1

--- AoC2024-3.py
+++ AoC2024-3.py
@@ -1,19 +1,25 @@
+import re
+
 testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
-validChars = ["m", "u", "l", "(", ")", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-puzzleInput = testInput
+puzzleInput = str(open("day3input").read())
 
-def removeInvalidChars(inputList):
-    validList = []
-    inputList = list(inputList)
-    for char in inputList:
-        if char in validChars:
-            validList.append(char)
-    validList = "".join(validList)
-    return validList
+def extractInstructions(string):
+    pattern = r"mul\(\d+,\d+\)"
+    instructions = re.findall(pattern, puzzleInput)
+    return instructions
 
-print(removeInvalidChars(puzzleInput))
+def numsFromInstructions(instructions):
+    for i in range(len(instructions)):
+        instructions[i] = (instructions[i][4:-1]).split(",")
+    return instructions
 
+def executeInstructions(garbledInstructions):
+    total = 0
+    instructions = extractInstructions(garbledInstructions)
+    nums = numsFromInstructions(instructions)
+    for pair in nums:
+        total += int(pair[0]) * int(pair[1])
+    return total
 
-
-
+print(executeInstructions(puzzleInput))
