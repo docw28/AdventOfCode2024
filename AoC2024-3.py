e6403eb98b6c90949ef21660c93f83fdb1d1d1d9
Dafydd Carver-Williams <d.carverwilliams@andor.com>
2024-12-17 09:58:12 +0000

    finished removeInvalidChars function for day 3

--- AoC2024-3.py
+++ AoC2024-3.py
@@ -1,14 +1,19 @@
 testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 validChars = ["m", "u", "l", "(", ")", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-puzzleInput = list(testInput)
+puzzleInput = testInput
 
-def removeInvalidChars(input):
-    output = []
-    for char in input:
+def removeInvalidChars(inputList):
+    validList = []
+    inputList = list(inputList)
+    for char in inputList:
         if char in validChars:
-            output.append(char)
-    return output
+            validList.append(char)
+    validList = "".join(validList)
+    return validList
 
 print(removeInvalidChars(puzzleInput))
+
+
+
 
