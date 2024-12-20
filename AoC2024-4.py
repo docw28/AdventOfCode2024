f3ac82ca8786a2991cf30fce8e6e9f3d572030a9
Dafydd Carver-Williams <d.carverwilliams@andor.com>
2024-12-20 11:14:37 +0000

    started day 4

--- AoC2024-4.py
+++ AoC2024-4.py
@@ -8,3 +8,29 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX""" # solution is 18
+
+def ingest(inputString):
+    inputList = []
+    ingestedString = []
+
+    inputList = inputString.split("\n")
+    for line in inputList:
+        ingestedString.append(list(line))
+    
+    return ingestedString
+
+def wordSearch(array, string):
+    total = 0
+    # look for each instance of the starting character
+    # check each direction for the next character
+    # if found, check the same direction for the next character
+    return total
+
+def ceresSearch(puzzleInput):
+    # puzzleInput = ingest(puzzleInput)
+    # answer = wordSearch(puzzleInput, "XMAS")
+    answer = 0
+
+    return answer
+
+print(ingest(testInput1))
