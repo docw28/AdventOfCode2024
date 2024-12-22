def115fa1f6ca78a9a9434f37f7c0db2cdc0256d
dcw00_cyberdeck <d.carverwilliams@gmail.com>
2024-12-22 19:34:55 +0000

    works with test, but not with full input

--- AoC2024-4.py
+++ AoC2024-4.py
@@ -9,6 +9,8 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX""" # solution is 18
 
+puzzleInput = str(open("day4input").read())
+
 vectors = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
 
 def ingest(inputString):
@@ -29,31 +31,23 @@ def wordSearch(array, string):
             if array[i][j] == string[0]: # string is "XMAS"
                 print("-", end="")
                 for vector in vectors:
-                    maxI = i + (vector[0] * (len(string))-1)
-                    maxJ = j + (vector[1] * (len(string))-1)
+                    maxI = i + (vector[0] * ((len(string))-1))
+                    maxJ = j + (vector[1] * ((len(string))-1))
+                    print(maxI,maxJ)
                     if (0 <= maxI < len(array)) and (0 <= maxJ < len(array[i])):
-                      print(i,j, end="")
                       check = ""
-                      for c in range(len(string):
+                      for c in range(len(string)):
                           check += array[i+(vector[0]*c)][j+(vector[1]*c)]
                       print(",", check, end="")
                       if check == string:
                           total += 1
-                            
-
-
-                
-                
-
-    # look for each instance of the starting character
-    # check each direction for the next character
-    # if found, check the same direction for the next character
     return total
 
-def ceresSearch(puzzleInput, string):
-    puzzleInput = ingest(puzzleInput)
-    answer = wordSearch(puzzleInput, string)
+def ceresSearch(Input, string):
+    Input = ingest(Input)
+    answer = wordSearch(Input, string)
     return answer
 
 print("\n", ceresSearch(testInput1, "XMAS"))
+print("\n", ceresSearch(puzzleInput, "XMAS"))
 
