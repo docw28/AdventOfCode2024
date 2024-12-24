ff492c8745e1bf682ccef116049309a9b118d6d1
Dafydd Carver-Williams <d.carverwilliams@andor.com>
2024-12-24 13:09:55 +0000

    finished day 4 (badly)

--- AoC2024-4.py
+++ AoC2024-4.py
@@ -1,3 +1,5 @@
+import math
+
 testInput1 = """MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -7,47 +9,69 @@ XXAMMXXAMA
 SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
-MXMXAXMASX""" # solution is 18
+MXMXAXMASX""" # solution 1 is 18, solution 2 is 9
 
-puzzleInput = str(open("day4input").read()).strip()
-
-vectors = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+puzzleInput = str(open("day4input").read())
 
 def ingest(inputString):
+    # converts a string with linebreaks into a 2D array
     inputList = []
     ingestedString = []
 
-    inputList = inputString.split("\n")
+    inputList = inputString.strip().split("\n")
     for line in inputList:
         ingestedString.append(list(line))
         print(list(line))
     return ingestedString
 
-def wordSearch(array, string):
+def weFindAnyChar(gridOfLetters, desiredChar):
+    # returns a list of every location of that character in a 2D array
+    coOrds = []
+    for l in range(len(gridOfLetters)):
+        for c in range(len(gridOfLetters[l])):
+            if gridOfLetters[l][c] == desiredChar:
+                coOrds.append([l,c])
+    return coOrds
+
+def hasMAS(array, position):
+    if (array[position[0]-1][position[1]-1] == "M") and (array[position[0]-1][position[1]+1] == "M") and (array[position[0]+1][position[1]+1] == "S") and (array[position[0]+1][position[1]-1] == "S"):
+        return True
+    if (array[position[0]-1][position[1]-1] == "S") and (array[position[0]-1][position[1]+1] == "M") and (array[position[0]+1][position[1]+1] == "M") and (array[position[0]+1][position[1]-1] == "S"):
+        return True
+    if (array[position[0]-1][position[1]-1] == "S") and (array[position[0]-1][position[1]+1] == "S") and (array[position[0]+1][position[1]+1] == "M") and (array[position[0]+1][position[1]-1] == "M"):
+        return True
+    if (array[position[0]-1][position[1]-1] == "M") and (array[position[0]-1][position[1]+1] == "S") and (array[position[0]+1][position[1]+1] == "S") and (array[position[0]+1][position[1]-1] == "M"):
+        return True
+    return False
+
+def wordSearch(inputString, desiredWord):
     total = 0
-    for i in range(len(array)):
-        # print("\n", end="")
-        for j in range(len(array[i])):
-            if array[i][j] == string[0]: # string is "XMAS"
-                # print("-", end="")
-                for vector in vectors:
-                    maxI = i + (vector[0] * ((len(string))-1))
-                    maxJ = j + (vector[1] * ((len(string))-1))
-                    # print(maxI,maxJ)
-                    if (0 <= maxI < len(array)) and (0 <= maxJ < len(array[i])):
-                      check = ""
-                      for c in range(len(string)):
-                          check += array[i+(vector[0]*c)][j+(vector[1]*c)]
-                      # print(",", check, end="")
-                      if check == string:
-                          total += 1
+    vectors = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+    inputArray = ingest(inputString)
+    starters = weFindAnyChar(inputArray, desiredWord[0])
+    for position in starters:
+        for vector in vectors:
+            maxL = position[0] + (vector[0] * (len(desiredWord)-1))
+            maxC = position[1] + (vector[1] * (len(desiredWord)-1))
+            if (0 <= maxL < len(inputArray)) and (0 <= maxC < len(inputArray[position[0]])):
+                check = ""
+                for c in range(len(desiredWord)):
+                    check += inputArray[position[0]+(vector[0]*c)][position[1]+(vector[1]*c)]
+                if check == desiredWord:
+                    total += 1
     return total
 
-def ceresSearch(Input, string):
-    Input = ingest(Input)
-    answer = wordSearch(Input, string)
-    return answer
+def crossSearch(inputString, desiredWord):
+    total = 0
+    test = []
+    inputArray = ingest(inputString)
+    starters = weFindAnyChar(inputArray, desiredWord[math.floor(len(desiredWord)/2)])
+    for position in starters:
+        if (1 <= position[0] < (len(inputArray)-1)) and (1 <= position[1] < (len(inputArray[position[0]])-1)) and hasMAS(inputArray, position):
+            total += 1
+    return total
 
-print("\n", ceresSearch(testInput1, "XMAS"))
-print("\n", ceresSearch(puzzleInput, "XMAS"))
-
+# print(wordSearch(testInput1, "XMAS"))
+# print(crossSearch(testInput1, "MAS"))
+# print(wordSearch(puzzleInput, "XMAS"))
+print(crossSearch(puzzleInput, "MAS"))
